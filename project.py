0f413be37f7e327969bb0a970ab78222109f7a36
wma-coder <williamma@college.harvard.edu>
2026-05-01 19:09:19 +0000

    Fri, May 1, 2026, 3:09 PM -04:00

--- project.py
+++ project.py
@@ -23,7 +23,7 @@ class GolfBag:
 
         for i in range(num_clubs):
             club = input("Enter club name: ")
-            distance = int(input(f"How far do you hit your {club} at sea level (yards)? "))
+            distance = int(input(f"How far do you hit your {club} at sea level (yards) in 70° F? "))
             self.clubs[club] = distance
 
     def show_bag(self):
